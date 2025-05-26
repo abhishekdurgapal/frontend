b275b5eb830d71bcce56a4f9d4642a6a30b706d5
abhishekdurgapal <abhishekdurgapal367@gmail.com>
2025-05-26 22:46:26 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const API = 'http://localhost:4000';
+const API = process.env.REACT_APP_API_URL;
 
 export default function Login() {
   const [form, setForm] = useState({ aadharCardNumber: '', password: '' });
